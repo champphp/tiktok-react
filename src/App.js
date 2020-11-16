28767f59b6ce6bca9806593b5dbba647e01766bb
ChampPK <thechamp2535@gmail.com>
2020-11-17 01:57:48 +0700

    connect frontend and backend

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
 import Video from './video/Video'
+import axios from './axios'
 
 function App() {
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const resp = await axios.get('/v2/posts')
+      setVideos(resp.data)
+    }
+
+    fetchPosts()
+  }, [])
   return (
     <div className="App">
       <div className="app_videos">
-        <Video 
-          url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4" 
-          channel="sssOne"
-          description="Learn the MERN Stack - Full Tutorial for Beginners"
-          song="Take on me"
-          likes={111}
-          messages={200}
-          shares={20}
-        />
-        <Video 
-          url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4" 
-          channel="ssssTow"
-          description="React Material ready to use from the official website"
-          song="Black in black"
-          likes={200}
-          messages={500}
-          shares={100}
-        />
+        {videos.map(
+          ({url, channel, description, song, likes, messages, shares}, index) => (
+          <Video
+            key={index}
+            url={url}
+            channel={channel}
+            description={description}
+            song={song}
+            likes={likes}
+            messages={messages}
+            shares={shares}
+          />
+        ))}
       </div>
-      {/* app container */}
-      {/* videos */}
+      
     </div>
   );
 }
